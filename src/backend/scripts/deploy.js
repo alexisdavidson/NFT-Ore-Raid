dc17ad3ffe2350d1833084d67866de348898f2ca
Alexis Davidson <alexisdavidson00@gmail.com>
2022-11-10 16:02:58 +0800

    Add goerli team wallet

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -7,8 +7,8 @@ async function main() {
   // Fill with correct data and uncomment the correct network before deploy!
   // const teamWallet = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"; // localhost
   // const whitelistedAddresses = ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"]; // localhost
-  const teamWallet = "0xD71E736a7eF7a9564528D41c5c656c46c18a2AEd"; // goerli
-  const whitelistedAddresses = ["0xD71E736a7eF7a9564528D41c5c656c46c18a2AEd"]; // goerli
+  const teamWallet = "0x7327ccC29B7cd5f0Bdc1957c5E32BE1f6Bf31446"; // goerli
+  const whitelistedAddresses = ["0xD71E736a7eF7a9564528D41c5c656c46c18a2AEd", "0x7327ccC29B7cd5f0Bdc1957c5E32BE1f6Bf31446"]; // goerli
   
   const NFT = await ethers.getContractFactory("NFT");
   const nft = await NFT.deploy(teamWallet, whitelistedAddresses);
